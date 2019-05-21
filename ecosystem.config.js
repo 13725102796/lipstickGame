759dae70e31befa6a44af92ba7ea3b97faa5f43a
张业昌 <1439655764@qq.com>
2019-05-21 14:54:36 +0800

    添加 pm2管理

---
+++ ecosystem.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+
+    // First application
+    {
+      name: 'API',
+      script: 'app.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    },
+
+    // Second application
+    {
+      name: 'WEB',
+      script: 'web.js'
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: 'root',
+      host: '129.204.194.110',
+      ref: 'origin/cdn',
+      repo: 'git@github.com:13725102796/lipstickGame.git',
+      path: '/zyc/lipstickGame',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev: {
+      user: 'node',
+      host: '129.204.194.110',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/development',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env: {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
